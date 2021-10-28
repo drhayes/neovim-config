26ccbc60f2dbd87487dcff3a9ccaa9744d9e5131
David Hayes <drhayes@gmail.com>
2021-10-27 23:55:57 -0500

    Buffer key mapping. Sorting things.

--- lua/drhayes/keyMappings.lua
+++ lua/drhayes/keyMappings.lua
@@ -1,30 +1,43 @@
 
 local keyMappings = {}
 
+local function bufferMaps()
+  return {
+    name = "+buffer",
+    b = { ':e#<CR>', "Last Buffer"},
+    d = { '<Cmd>BufferLineSortByDirectory<CR>', "Order by Dir" },
+    g = { "<cmd>BufferLinePick<cr>", "Goto Buffer" },
+    l = { '<Cmd>BufferLineSortByDirectory<CR>', "Order by Lang" },
+    n = { "<Cmd>BufferLineCycleNext<CR>", "Next Buffer" },
+    p = { "<Cmd>BufferLineCyclePrev<CR>", "Previous Buffer" },
+    s = { "<cmd>lua require('telescope.builtin').buffers()<CR>", "Buffers" },
+  }
+end
+
 local function fileMaps()
   return {
     name = '+file',
-    t = { "<cmd>NvimTreeToggle<cr>", "NvimTree" },
-    s = { ":SymbolsOutline<CR>", "Symbols" },
     f = { "<cmd>lua require('telescope.builtin').find_files()<CR>", "Find Files" },
     g = { "<cmd>lua require('telescope.builtin').git_files()<CR>", "Find Git Files" },
-    r = { "<cmd>lua require('telescope.builtin').oldfiles()<CR>", "Recent Files" },
     n = { "<cmd>enew<CR>", "New File" },
+    r = { "<cmd>lua require('telescope.builtin').oldfiles()<CR>", "Recent Files" },
+    s = { ":SymbolsOutline<CR>", "Symbols" },
+    t = { "<cmd>NvimTreeToggle<cr>", "NvimTree" },
   }
 end
 
 local function gitMaps()
   return {
     name = "+git",
-    g = { "<cmd>lua require('neogit').open()<cr>", "Neogit" },
-    w = { ":Gwrite<cr>", "Git Write" },
-    c = { "<Cmd>Telescope git_commits<CR>", "commits" },
     b = { "<Cmd>Telescope git_branches<CR>", "branches" },
-    s = { "<Cmd>Telescope git_status<CR>", "status" },
+    c = { "<Cmd>Telescope git_commits<CR>", "commits" },
     d = { "<cmd>DiffviewOpen<cr>", "DiffView" },
+    g = { "<cmd>lua require('neogit').open()<cr>", "Neogit" },
     h = { name = "+hunk" },
     i = { "<cmd>lua require('telescope').extensions.gh.issues()<CR>", "issues" },
     p = { "<cmd>lua require('telescope').extensions.gh.pull_request()<CR>", "pull requests" },
+    s = { "<Cmd>Telescope git_status<CR>", "status" },
+    w = { ":Gwrite<cr>", "Git Write" },
   }
 end
 
@@ -32,6 +45,7 @@ function keyMappings.initKeymap()
   local wk = require 'which-key'
 
   wk.register({
+    b = bufferMaps(),
     f = fileMaps(),
     g = gitMaps(),
   }, { prefix = '<leader>' })
