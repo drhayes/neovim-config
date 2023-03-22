40b0c04b65af8f63d45e32a8cf837dc44e396e93
David Hayes <drhayes@omg.lol>
2023-03-22 16:09:07 -0500

    Try to get everything to use the latest version of node.
    Fixed my problem with crashing LSPs!

--- init.lua
+++ init.lua
@@ -1,2 +1,10 @@
+-- First things first, set the node path globally.
+if vim.fn.has("unix") and vim.env.NEOVIM_NODE_VERSION then
+  local node_dir = vim.env.HOME .. "/.asdf/installs/nodejs/" .. vim.env.NEOVIM_NODE_VERSION .. "/bin/"
+  if vim.fn.isdirectory(node_dir) then
+    vim.env.PATH = node_dir .. ":" .. vim.env.PATH
+  end
+end
+
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
