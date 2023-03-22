47af08ded4b7805417d01535fa4fdb804307559b
David Hayes <drhayes@omg.lol>
2023-03-22 00:30:22 -0500

    Prettier for formatting CSS!

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -54,6 +54,24 @@ return {
   },
 
   {
+    "jose-elias-alvarez/null-ls.nvim",
+    opts = function()
+      local nls = require("null-ls")
+      return {
+        root_dir = require("null-ls.utils").root_pattern(".null-ls-root", ".neoconf.json", "Makefile", ".git"),
+        sources = {
+          nls.builtins.formatting.prettier,
+          nls.builtins.formatting.fish_indent,
+          nls.builtins.diagnostics.fish,
+          nls.builtins.formatting.stylua,
+          nls.builtins.formatting.shfmt,
+          nls.builtins.diagnostics.flake8,
+        },
+      }
+    end,
+  },
+
+  {
     "neovim/nvim-lspconfig",
     opts = {
       servers = {
