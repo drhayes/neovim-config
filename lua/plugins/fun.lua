ee37d6ca542d89de1da7348534adc99e3d61fc58
David Hayes <drhayes@omg.lol>
2024-04-23 10:39:13 -0500

    No more octo.nvim, wasn't using it anyway.

--- lua/plugins/fun.lua
+++ lua/plugins/fun.lua
@@ -4,4 +4,16 @@ return {
   --   dependencies = { 'MunifTanjim/nui.nvim', 'nvim-lua/plenary.nvim' },
   --   opts = {},
   -- },
+  {
+    'michaelrommel/nvim-silicon',
+    lazy = true,
+    cmd = 'Silicon',
+    config = function()
+      require('silicon').setup({
+        font = 'JetBrains Mono=16',
+        theme = 'Dracula',
+        to_clipboard = true,
+      })
+    end,
+  },
 }
