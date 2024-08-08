426fd235e6cd3d4f41b4d0701a48eeb319330a9a
David Hayes <drhayes@omg.lol>
2024-08-08 13:57:58 -0500

    Add octo.

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -11,4 +11,21 @@ return {
     },
     config = true,
   },
+
+  {
+    'pwntester/octo.nvim',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      'nvim-telescope/telescope.nvim',
+      -- OR 'ibhagwan/fzf-lua',
+      'nvim-tree/nvim-web-devicons',
+    },
+    config = function()
+      require('octo').setup({
+        suppress_missing_scope = {
+          projects_v2 = true,
+        },
+      })
+    end,
+  },
 }
