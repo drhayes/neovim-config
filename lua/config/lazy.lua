ab973f0b97446cb2c40a7d192dc07f571d8ae5e0
David Hayes <drhayes@omg.lol>
2025-10-06 15:05:59 -0500

    Catching up with Lazy.

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -13,7 +13,7 @@ require('lazy').setup({
     -- import any extras modules here
     { import = 'lazyvim.plugins.extras.lang.typescript' },
     { import = 'lazyvim.plugins.extras.lang.json' },
-    { import = 'lazyvim.plugins.extras.editor.leap' },
+    -- { import = 'lazyvim.plugins.extras.editor.leap' },
     -- { import = 'lazyvim.plugins.extras.lang.markdown' },
     { import = 'lazyvim.plugins.extras.linting.eslint' },
     { import = 'lazyvim.plugins.extras.formatting.prettier' },
