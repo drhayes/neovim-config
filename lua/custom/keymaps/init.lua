08f4a1acf5195f9bca256f195dfd4686de7222c0
David Hayes <drhayes@omg.lol>
2025-10-07 14:14:53 -0500

    Pulling out keymaps. Ensuring some LSPs get installed.

---
+++ lua/custom/keymaps/init.lua
@@ -0,0 +1,51 @@
+-- local wk = require('which-key')
+-- print(wk)
+
+local function map(mode, lhs, rhs, opts)
+  vim.keymap.set(mode, lhs, rhs, opts)
+  -- local keys = require('lazy.core.handler').handlers.keys
+  -- ---@cast keys LazyKeysHandler
+  -- -- do not create the keymap if a lazy keys handler exists
+  -- if not keys.active[keys.parse({ lhs, mode = mode }).id] then
+  --   opts = opts or {}
+  --   opts.silent = opts.silent ~= false
+  --   vim.keymap.set(mode, lhs, rhs, opts)
+  -- end
+end
+
+-- Clear highlights on search when pressing <Esc> in normal mode
+--  See `:help hlsearch`
+map('n', '<Esc>', '<cmd>nohlsearch<CR>')
+
+-- Diagnostic keymaps
+map('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
+
+-- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
+-- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
+-- is not what someone will guess without a bit more experience.
+--
+-- NOTE: This won't work in all terminal emulators/tmux/etc. Try your own mapping
+-- or just use <C-\><C-n> to exit terminal mode
+map('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
+
+-- Keybinds to make split navigation easier.
+--  Use CTRL+<hjkl> to switch between windows
+map('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
+map('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
+map('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
+map('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+
+-- Move blocks when visually selected.
+map('v', 'J', ":m '>+1<CR>gv=gv")
+map('v', 'K', ":m '<-2<CR>gv=gv")
+
+-- Center after move.
+map('n', 'n', 'nzzzv', { desc = 'Next then center' })
+map('n', 'N', 'Nzzzv', { desc = 'Prev then center' })
+map('n', 'G', 'Gzz', { desc = 'Go then center' })
+
+-- NOTE: Some terminals have colliding keymaps or are not able to send distinct keycodes
+-- map("n", "<C-S-h>", "<C-w>H", { desc = "Move window to the left" })
+-- map("n", "<C-S-l>", "<C-w>L", { desc = "Move window to the right" })
+-- map("n", "<C-S-j>", "<C-w>J", { desc = "Move window to the lower" })
+-- map("n", "<C-S-k>", "<C-w>K", { desc = "Move window to the upper" })
