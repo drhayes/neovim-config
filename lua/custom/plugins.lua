aac3d787b27fb6e7bc0133e9dedbc6d2254338c7
David Hayes <drhayes@omg.lol>
2025-10-22 09:57:36 -0500

    Add comment about openingh not working.

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -628,11 +628,17 @@ return {
     -- this is equivalent to setup({}) function
   },
 
+  -- As of 2025-10-22, this doesn't seem to be working:
+  -- Error executing Lua callback: ...cal/share/nvim/lazy/openingh.nvim/lua/openingh/utils.lua:218: attempt to concatenate local 'url' (a nil value)
+  -- stack traceback:
+  -- 	...cal/share/nvim/lazy/openingh.nvim/lua/openingh/utils.lua:218: in function 'open_url'
+  -- 	...ocal/share/nvim/lazy/openingh.nvim/lua/openingh/init.lua:98: in function 'open_url'
+  -- 	....local/share/nvim/lazy/openingh.nvim/plugin/openingh.lua:67: in function <....local/share/nvim/lazy/openingh.nvim/plugin/openingh.lua:63>
   {
     'almo7aya/openingh.nvim',
     keys = {
       { '<leader>gr', '<cmd>OpenInGHRepo <cr>', desc = 'Open in GitHub repo' },
-      { '<leader>gf', '<cmd>OpenInGHFile <cr>', desc = 'Open in GitHub file' },
+      { '<leader>gR', '<cmd>OpenInGHFile <cr>', desc = 'Open in GitHub file' },
     },
   },
 
