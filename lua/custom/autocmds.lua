e026d68bc07b4ed5bc159634ee1a54b9ae3cb550
David Hayes <drhayes@omg.lol>
2026-02-05 09:38:17 -0600

    Split the window vertically with help instead.

--- lua/custom/autocmds.lua
+++ lua/custom/autocmds.lua
@@ -9,6 +9,12 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   end,
 })
 
+-- Make help open in a horizontal split.
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = 'help',
+  command = 'wincmd L',
+})
+
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
