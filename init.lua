d78408a8f4921c2a70a3d4e825ec24bf4cae048e
David Hayes <drhayes@omg.lol>
2026-02-17 14:38:27 -0600

    Make sure gitcommit is among installed treesitter parsers.

--- init.lua
+++ init.lua
@@ -698,8 +698,20 @@ require('lazy').setup({
     lazy = false,
     build = ':TSUpdate',
     config = function()
-      local filetypes =
-        { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc' }
+      local filetypes = {
+        'bash',
+        'c',
+        'diff',
+        'gitcommit',
+        'html',
+        'lua',
+        'luadoc',
+        'markdown',
+        'markdown_inline',
+        'query',
+        'vim',
+        'vimdoc',
+      }
       require('nvim-treesitter').install(filetypes)
       vim.api.nvim_create_autocmd('FileType', {
         pattern = filetypes,
