d8d3f7c441f3ef99c8eb0c747a15760e46e0ade9
David Hayes <drhayes@omg.lol>
2026-04-01 10:19:20 -0500

    epiquery, octo, coffee-script (sigh)

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -814,4 +814,49 @@ return {
   --     --     vim.keymap.set('n', '-', '<C-x>', { desc = 'Decrement under cursor', noremap = true })
   --   end,
   -- },
+  {
+    'git@github.com:glg/epiquery.nvim.git',
+    opts = {
+      epiquery_url = 'http://localhost:9090',
+    },
+  },
+
+  {
+    'pwntester/octo.nvim',
+    cmd = 'Octo',
+    opts = {
+      -- or "fzf-lua" or "snacks" or "default"
+      picker = 'snacks',
+      -- bare Octo command opens picker of commands
+      enable_builtin = true,
+    },
+    init = function()
+      local wk = require('which-key')
+      wk.add({
+        { '<leader>o', group = 'Octo', mode = 'n' },
+        { '<leader>oi', '<cmd>Octo issue list<cr>', desc = 'List GitHub [I]ssues' },
+        { '<leader>op', '<cmd>Octo pr list<cr>', desc = 'List GitHub [P]ullRequests' },
+        { '<leader>od', '<cmd>Octo discussion list<cr>', desc = 'List GitHub [D]iscussions' },
+        { '<leader>on', '<cmd>Octo notification list<cr>', desc = 'List GitHub [N]otifications' },
+        {
+          '<leader>os',
+          function()
+            require('octo.utils').create_base_search_command({ include_current_repo = true })
+          end,
+          desc = '[S]earch GitHub',
+        },
+      })
+    end,
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      -- 'nvim-telescope/telescope.nvim',
+      -- OR "ibhagwan/fzf-lua",
+      'folke/snacks.nvim',
+      'nvim-tree/nvim-web-devicons',
+    },
+  },
+
+  {
+    'kchmck/vim-coffee-script',
+  },
 }
